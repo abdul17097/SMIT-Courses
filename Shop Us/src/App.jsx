cdacbba8137115a3b78ff81112d219a4fb200a31
abdulmusavir17097 <abdul.musavir@barqbytes.com>
2025-01-05 14:01:11 +0500

    shop us

---
+++ Shop Us/src/App.jsx
@@ -0,0 +1,14 @@
+import { Carousel } from './components/ui/carousel'
+import { CarouselPlugin } from './customComponents/HeroSection'
+import Navabar from './customComponents/Navabar'
+
+function App() {
+  return (
+    <>
+      <Navabar />
+      <CarouselPlugin/>
+    </>
+  )
+}
+
+export default App
