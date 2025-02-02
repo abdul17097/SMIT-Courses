8f52e57b055c201b285923615e8a187d75e5bb54
abdul17097 <abdul17097@gmail.com>
2025-02-02 17:03:16 +0500

    start javascript

---
+++ WEB Batch 3 section F/Javascipt/index.js
@@ -0,0 +1,41 @@
+// alert('hello "world"');
+var myname = "abdul";
+
+var roll = 17097; //initialization & declaration
+myname = "musavir"; // reassign or initialize
+
+console.log("my name is " + myname + " and Roll is " + roll);
+
+// Marks
+var english_Marks; // declare
+english_Marks = 100; // initialize
+
+// document.write("English marks " + english_Marks + "\n");
+
+var firstName = "abdul";
+var lastName = "musavir";
+
+var fullName = firstName + lastName;
+// document.write("Full name " + fullName);
+
+var math = 50;
+var ur = 90;
+var pak = 12;
+// var math = "50";
+// var ur = "90";
+// var pak = "12";
+
+var totalMarks = math + ur + pak;
+
+// document.write("\nTotal marks " + totalMarks); //509012
+
+var averageMarks = totalMarks + 2;
+var bio = 90;
+
+var totalMarksAndBio = totalMarks + bio;
+
+// document.write("\nAverage marks " + totalMarksAndBio);
+
+var hello123 = "aslkdf";
+document.write(hello123);
+window.alert(234);
