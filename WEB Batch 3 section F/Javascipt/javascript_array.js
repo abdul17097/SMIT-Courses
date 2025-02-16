deb4e2255d8d424c096d59bbdf8b60009f512f6d
abdul17097 <abdul17097@gmail.com>
2025-02-16 21:19:25 +0500

    array

---
+++ WEB Batch 3 section F/Javascipt/javascript_array.js
@@ -0,0 +1,75 @@
+var cars = ["test1", "test2", "test3"];
+// console.log(cars);
+
+// "" empty string
+// '' empty string
+// [] empty array
+
+// var userNames = [];
+// userNames[0] = "Amaar";
+// userNames[3] = "Amaar123";
+
+// console.log(userNames[0]);
+// console.log(userNames[1]);
+// console.log(userNames[2]);
+// console.log(userNames[3]);
+// console.log(userNames);
+
+// var multipleData = ["one", 2, "three", true, 3.4];
+// console.log(multipleData);
+
+// var fruits = new Array("hello", true);
+
+// fruits[0] = "apple";
+// fruits[0] = "orange";
+// console.log(fruits);
+// document.write(fruits);
+// alert(fruits[[0]]);
+// remove item from beginning of an array
+// fruits.shift();
+// fruits.shift();
+// console.log(fruits);
+
+// document.write(fruits);
+
+// var students = ["jhon"]; // new Array ()
+// // students.unshift("jhon123");
+// students.unshift("jhon123", "waseem", "waseem123");
+// console.log(students[2]);
+
+// console.log(students);
+
+// var names = ["dog", "cat", "fly", "bug", "ox"];
+// var names = ["dog", "bug", "ox"];
+// remove specific item from as array
+// names.splice(2, 1);
+// names.splice(1);
+// console.log(names);
+
+// var firstArray = [1, 3, 4, 5, 5.5];
+// var secondArray = [6, 7, 8, 9, "hello"];
+
+// var thirdArray = secondArray.concat(firstArray, secondArray);
+
+// var input = prompt("Find you number:");
+
+// var result;
+
+// if (typeof input === "string") {
+//   result = thirdArray.includes(input);
+// } else {
+//   input = Number(input);
+//   result = thirdArray.includes(input);
+// }
+
+// if (result === true) {
+//   alert("Number found!");
+// } else {
+//   alert("Number not found!");
+// }
+// console.log(thirdArray);
+
+// var firstArray = [1, 3, 4, 5, "hello", 5.5, 6, "world", 7, 8, 9, 10];
+// console.log(firstArray.indexOf("worldslksfj"));
+
+// console.log(firstArray.join("9"));
