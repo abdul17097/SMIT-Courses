bdf38b6ce164959c8edd9a0485cffb64a9b80b5f
abdul17097 <103925027+abdul17097@users.noreply.github.com>
2025-07-21 22:38:37 +0500

    Add files via upload

---
+++ Batch 2 Section/index.js
@@ -0,0 +1,64 @@
+import express from "express";
+import { errorHandling } from "./middleware/errorMiddleware.js";
+import { generateToken } from "./utils/generateToken.js";
+import { verifyToken } from "./middleware/verify.js";
+
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res, next) => {
+  try {
+    res.json({
+      message: "hello world",
+    });
+  } catch (error) {
+    next(new Error(error));
+  }
+});
+
+app.get("/user-profile", verifyToken, (req, res) => {
+  res.json(userDetails);
+});
+
+// Database
+const userDetails = {
+  id: 134435,
+  email: "test123@gmail.com",
+  password: "test123",
+};
+
+// Login API
+app.post("/login", (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const { email: userEmail, password: userPassword } = userDetails;
+    if (!email || !password) {
+      return res.json({
+        message: "Please fill all Fields",
+        success: false,
+      });
+    }
+
+    if (userEmail !== email || userPassword !== password) {
+      return res.json({
+        message: "Invalid Credentials",
+        success: false,
+      });
+    }
+
+    const jwtToken = generateToken(userDetails);
+    res.json({
+      message: "User Login Successfully",
+      success: true,
+      token: jwtToken,
+    });
+  } catch (error) {
+    next(new Error(error));
+  }
+});
+
+app.use(errorHandling);
+
+app.listen(5000, () => {
+  console.log("Server running on port:5000");
+});
