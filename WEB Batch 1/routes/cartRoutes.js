daa4c34cdee3204b5fc1f6af5e28b8b03cefaab5
abdul17097 <103925027+abdul17097@users.noreply.github.com>
2025-07-21 22:26:08 +0500

    Delete WEB Batch 1 directory

--- WEB Batch 1/routes/cartRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { verifyUser } from "../middleware/verifyUser.js";
-import { addToCart, cartData } from "../controller/cartController.js";
-const routes = express.Router();
-
-routes.post("/addToCart", verifyUser, addToCart);
-routes.get("/cart", verifyUser, cartData);
-
-export default routes;
--- WEB Batch 1/routes/paymentRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { payment } from "../controller/paymentController.js";
-const routes = express.Router();
-
-routes.post("/checkout", payment);
-
-export default routes;
