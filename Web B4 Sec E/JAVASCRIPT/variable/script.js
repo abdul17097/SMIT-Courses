608922f9df99ad6acba63e813a53fb433bd804b6
abdul17097 <abdul17097@gmail.com>
2025-11-08 15:02:02 +0500

    variable | dataypes | syntax | variable rules

---
+++ Web B4 Sec E/JAVASCRIPT/variable/script.js
@@ -0,0 +1,126 @@
+// let:
+// Decleared
+// not Recleared
+// Initialize
+// Reassign
+
+
+// var:
+// Decleared
+// Recleared
+// Initialize
+// Reassign
+
+// const:
+// Not Decleared
+// Not Recleared
+// Not Initialize
+// Not Reassign
+// Decleartaion & Initialization
+
+// var name; // variable declaration
+// name = "abdul";// variable Initialization
+
+// console.log(name); 
+
+// var name = "test"; 
+// name = "test123"; // reassign
+// console.log(name);
+
+
+// let fname;
+// fname = "practice";
+// fname = "practice123";
+
+// console.log(fname);
+
+// let lname = "best";
+
+// console.log(lname);
+
+// const full_name = "abdul musavir";
+// console.log("your fullname is: " + full_name);
+
+
+// Variable Rules
+// let first name = "test 123"
+// let first_name = "test 123";
+// let firstName = "test 123";
+// console.log(firstName);
+
+// // let  = "lkasjd";
+// = : assignment operator
+// let 123test = "";
+
+// let a = 12;
+// let b = 14;
+// // let result = 12 + 14;
+// let result = a + b; // 26
+// let c = result + 5 + a + b; // expression
+// console.log(c);
+
+// '': single quotes
+// "": Double //
+
+// String Datatype
+// let description =  "Lorem 'ipsum' dolor sit amet consectetur adipisicing elit. Earum, molestias quidem"; 
+
+// // typeof: to find data type
+// console.log(description);
+// console.log(typeof description);
+
+// // Number Datatype
+// let gpa = 3.14;
+// let roll_number = -234234;
+
+// console.log(gpa);
+// console.log(typeof gpa);
+// console.log(roll_number);
+// console.log(typeof roll_number);
+
+// // BigInt
+// let first_number = 387598237405827304895732n;
+// let second_number = 387598237405827304895732n;
+// let second_result = first_number + second_number
+// console.log(typeof second_result);
+
+
+// // undefined 
+// let version;
+// console.log(version);
+// console.log(typeof undefined);
+
+// null 
+let f_name = null;
+console.log(typeof f_name);
+console.log(f_name);
+f_name = "test";
+console.log(f_name);
+
+console.log(typeof null);
+
+  
+let kuchbi = "";
+kuchbi = "asdfasdf"
+console.log(typeof kuchbi);
+
+let student_details = [
+    {
+        id: Symbol("id"),
+
+    },
+    {
+        id: Symbol("id")
+
+    },
+    {
+        id: Symbol("id")
+
+    }
+]
+
+console.log(student_details);
+
+
+
+
