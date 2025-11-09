429351bb7af6b1b67ca7a25b9df8e3e8085f7081
abdul17097 <abdul17097@gmail.com>
2025-11-09 15:01:26 +0500

    arthematic | logical | comparsion | assignme operators

---
+++ Web B4 Sec E/JAVASCRIPT/operator/script.js
@@ -0,0 +1,106 @@
+//  Arthematic Operator
+// console.log("hello world");
+// let a = 2;
+// let b = 3;
+// add operator
+// console.log(a+b);
+
+// // subtract //
+// console.log(b-a);
+
+// // Division //
+// console.log(a/b);
+
+// // Multiplication //
+// console.log(a*b);
+
+// // ** : Power 
+// console.log(a ** 5);
+
+// // BDMAS: Bracket Division Multiplication Addition Subtraction 
+
+// let result = (2*3)+6/2-8; // Expresions
+// console.log(result);
+// b= 2;
+// Comparsion Operators
+// > : Grater Then
+// console.log(a > b); // false 
+// <: Less then
+// console.log(a < b); // true
+// >= : Grater Then or Equal
+// <= : Less Then or Equal
+// console.log(a <= b);
+
+// Equal
+// b = "2"; // reassigned
+// console.log(typeof b); // string
+// console.log(typeof a); // number
+// console.log("result:" + a+b); 
+
+// console.log( a == b); // true
+// console.log( a === b); // false
+
+// % : Modulas
+// b = 5;
+// console.log(b%a);
+// console.log(b);
+// b++; // add 1
+// b = b + 1
+// b = b + 2
+
+// b--; // subtract 1
+// b = b - 1
+// b = b - 2
+// console.log(b);
+// b= "2";
+
+// console.log(a !== b);
+
+// ++b
+// b +=3
+// b = b + 3
+
+// console.log(b);
+// b++
+// console.log(b);
+
+// b+=3
+// console.log(b);
+ 
+
+
+
+
+
+
+let x = 20;
+let y = 24;
+let age = 23;
+
+// t: true f: false
+// t t = t 
+// t f = f  
+// f t = f 
+// f f = f
+
+// && and ||
+// t t = t : &&
+// t f = t : ||
+// f t = t : ||
+
+// && :logical and Operator
+// t t = t : &&
+console.log((age > x) && (age < y));
+
+console.log((age > x) || (age < y));
+
+
+
+
+
+
+
+
+
+
+
