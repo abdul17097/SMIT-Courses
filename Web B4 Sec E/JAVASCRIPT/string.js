b5d7e853e9fb57fb0c98640b694f9e8a74f238f1
abdul17097 <abdul17097@gmail.com>
2025-12-13 14:54:46 +0500

    string method

---
+++ Web B4 Sec E/JAVASCRIPT/string.js
@@ -0,0 +1,50 @@
+// let fullName = "Muhammad Ayan";
+
+// console.log(fullName.length);
+// last index
+// console.log(fullName.charAt(4));
+// console.log(fullName.charAt(fullName.length - 1));
+// console.log(fullName.at(-2));
+// console.log(fullName[8]);
+
+// Cancatination
+let firstName = "Muhammad /#ulk898";
+let lastName = "Ayan ";
+let test = "ASDasd";
+
+let fullName = firstName.concat(lastName);
+// console.log(fullName);
+
+// console.log(fullName.slice(0, 5));
+// console.log(fullName.slice(0, 5));
+
+// console.log(fullName.substring(7));
+
+// console.log(fullName.toUpperCase());
+// console.log(fullName.toLowerCase());
+// let text = "     Hello World   ";
+// console.log(text.isWellFormed());
+// console.log(text);
+
+// console.log(text.trim());
+// console.log(text.trimStart());
+// console.log(text.trimEnd());
+// let marks = "12";
+// console.log(marks);
+
+// console.log(marks.padStart(3, "x"));
+// console.log(marks.padEnd(3, "x"));
+
+// let text = "Welcom to SMIT! and SMIT! ";
+// console.log(text.repeat(4));
+// let newText = text.replace("SMIt!", "Aziz Jan Trust!");
+// case Insensative
+// let newText = text.replace(/SMIt!/i, "Aziz Jan Trust!");
+// let newText = text.replace(/SMIT!/g, "Aziz Jan Trust!");
+// let newText = text.replaceAll("SMIT!", "Aziz Jan Trust!");
+// let newText = text.split(" ");
+
+// console.log(newText);
+
+let text = "The rain in SPAIN stays mainly in the plain";
+console.log(text.match("ain"));
