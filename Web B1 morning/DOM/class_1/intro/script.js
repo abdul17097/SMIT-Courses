51b549fad7720fdaad4752262139a56106077c59
abdul17097 <abdul17097@gmail.com>
2026-01-13 11:05:15 +0500

    DOM intro

---
+++ Web B1 morning/DOM/class_1/intro/script.js
@@ -0,0 +1,15 @@
+let heading = document.getElementById("heading");
+heading.style.color = "blue";
+heading.innerHTML = "welcome to Faheem";
+
+// console.log(heading);
+console.log(heading.innerHTML);
+
+function changeText() {
+  let para = document.getElementById("para");
+  para.innerHTML = "lkasjdlfk laksdjf";
+  para.style.color = "red";
+  para.style.fontSize = "25px";
+  para.style.backgroundColor = "black";
+  para.style.width = "40%";
+}
