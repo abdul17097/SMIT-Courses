59c9e493c307477d4a560befea3cb0ff1d2fa764
abdul17097 <abdul17097@gmail.com>
2026-01-31 15:03:25 +0500

    into to typescript

---
+++ Web B4 Sec E/Typescript/intro/intro.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 5;
+console.log(a);
+var test = "Test Name";
+test = "test 124";
+console.log("test");
+test = "laksdjf";
+var isActive;
+isActive = false;
+console.log(isActive);
+// let b = "salkdj";
+// b = 12;
+var numbers = [true, false, true];
+var names = ["true"];
+var abc = "asdklfj";
+// abc = {}
+var userDetails = {
+    name: "string",
+    age: 18,
+    section: "F",
+};
+console.log(userDetails.name);
---
+++ Web B4 Sec E/Typescript/intro/intro.ts
@@ -0,0 +1,32 @@
+let a: number = 5;
+console.log(a);
+
+let test: string = "Test Name";
+
+test = "test 124";
+console.log("test");
+
+test = "laksdjf";
+
+let isActive: boolean;
+isActive = false;
+console.log(isActive);
+
+// let b = "salkdj";
+// b = 12;
+
+let numbers: boolean[] = [true, false, true];
+let names: string[] = ["true"];
+
+let abc: any = "asdklfj";
+
+// abc = {}
+
+// let userDetails = {
+// };
+
+// userDetails.name = "23423";
+// console.log(userDetails.name);
+
+let cba: number;
+export {};
