4e73d5c39de5d581a9117ee1f91ef9004f685358
abdul17097 <abdul17097@gmail.com>
2026-05-02 15:47:41 +0500

    fs module and path

--- Web B4 Sec E/Backend/second-project/index.js
+++ Web B4 Sec E/Backend/second-project/index.js
@@ -1,4 +1,6 @@
+const { error } = require("console");
 const fs = require("fs");
+const path = require("path");
 
 // fs.writeFileSync("hello.txt", "welcom to FS Module!");
 
@@ -10,7 +12,52 @@ const fs = require("fs");
 // const data = fs.readFileSync("test.txt", "utf-8");
 // console.log(data);
 
-fs.readFile("hello.txt", "utf-8", (error, data) => {
-  if (error) console.log(error.message);
-  console.log(data);
+// fs.readFile("hello.txt", "utf-8", (error, data) => {
+//   if (error) console.log(error.message);
+//   console.log(data);
+// });
+
+// fs.appendFile("hello.txt", "\nthis is new data", (error) => {
+//   if (error) console.log(error);
+//   console.log("File Append Successfully!");
+// });
+
+// fs.unlink("test123.html", (error) => {
+//   if (error) console.log(error);
+//   console.log("File deleted Successfully!");
+// });
+
+// fs.mkdir("folder1", (error) => {
+//   if (error) console.log(error);
+//   console.log("Folder Created Successfully!");
+// });
+
+// fs.rmdir("folder1", (error) => {
+//   if (error) console.log(error);
+//   console.log("folder delted successfully!");
+// });
+
+// const newFolder = path.join(__dirname, "folder1");
+// const nestFolder = path.join(newFolder, "folder2");
+// const newFile = path.join(newFolder, "test.txt");
+
+// fs.mkdirSync(newFolder);
+// fs.mkdirSync(nestFolder);
+
+// fs.writeFile(newFile, "hello somthing", (error) => {
+//   if (error) console.log(error);
+//   console.log("File created Success!");
+// });
+
+const userDetail = {
+  id: 1,
+  name: "John Doe",
+  email: "john@gmail.co",
+};
+
+let userLog = `${new Date()} , login, Email: ${userDetail.email} \n`;
+
+fs.appendFile("logs.txt", userLog, (error) => {
+  if (error) console.log(error);
+  console.log("laksdjf");
 });
