c0a5c6fe658bda6e75106472a7833c48d8c659c3
abdul17097 <abdul17097@gmail.com>
2026-05-24 16:02:41 +0500

    cloudinary

--- Web B4 Sec E/Backend/sixth-project/index.js
+++ Web B4 Sec E/Backend/sixth-project/index.js
@@ -6,8 +6,11 @@ import { config } from "dotenv";
 import nodemailer from "nodemailer";
 import multer from "multer";
 import path from "path";
+import { cloudinary, cloudinaryConfig } from "./config/cloudinary.js";
+import { uploadBlog } from "./config/multer.js";
 
 config();
+cloudinaryConfig();
 const app = express();
 
 app.use(express.json());
@@ -56,6 +59,7 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+
     cb(
       null,
       file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname),
@@ -76,6 +80,37 @@ app.post("/uploadFile", upload.single("coverImage"), (req, res) => {
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", authRoutes);
+
+const uploadCloudinary = async (buffer) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream((error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+    stream.end(buffer);
+  });
+};
+
+app.post("/uploadBlog", uploadBlog.single("blogImage"), async (req, res) => {
+  const file = req.file;
+  const body = req.body;
+  if (!file) {
+    return res.status(400).json({
+      message: "No file uploaded",
+      success: false,
+    });
+  }
+
+  const result = await uploadCloudinary(file.buffer);
+  res.status(200).json({
+    message: "File uploaded successfully",
+    success: true,
+    data: result.secure_url,
+  });
+});
 
 // http://localhost:500/api/v1/products
 
