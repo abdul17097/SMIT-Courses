2a6ebee15b9dc65a8f8624cbe9c28f8849cec50a
abdul17097 <abdul17097@gmail.com>
2026-06-13 18:02:07 +0500

    module | propagation

---
+++ Sect A&B Bactch 4/Advanced Javascript/class_8/modules/calculator.js
@@ -0,0 +1,7 @@
+import { add } from "./add.js";
+import { multiply } from "./multiply.js";
+import { subtraction } from "./subtraction.js";
+
+console.log(add(1, 4));
+console.log(subtraction(1, 4));
+console.log(multiply(1, 4));
