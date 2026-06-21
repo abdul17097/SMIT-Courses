9c72c36b139d003f9448d32bdef0ba60c74a3375
abdul17097 <abdul17097@gmail.com>
2026-06-21 15:11:14 +0500

    admin apis

--- Web B4 Sec E/Backend/eigth-project/contollers/user.js
+++ Web B4 Sec E/Backend/eigth-project/contollers/user.js
@@ -1,4 +1,5 @@
 import { User } from "../modals/user.js";
+import bcrypt from "bcryptjs";
 
 export const allUser = async (req, res) => {
   try {
@@ -14,6 +15,102 @@ export const allUser = async (req, res) => {
       message: "All Users",
       data: users,
       success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const findUser = await User.findById(userId);
+
+    if (!findUser) {
+      return res.status(404).json({
+        message: "user not found",
+        success: false,
+      });
+    }
+
+    if (findUser.role == "admin") {
+      return res.status(403).json({
+        message: "You Can't delete this user",
+        success: false,
+      });
+    }
+
+    await User.deleteOne({
+      _id: userId,
+    });
+
+    res.status(200).json({
+      message: "User deleted Successfully!",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const findUser = await User.findById(userId);
+    if (!findUser) {
+      return res.status(404).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    res.status(200).json({
+      message: "User Details",
+      data: findUser,
+      scuccess: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { username, email, password } = req.body;
+
+    const findUser = await User.findById(userId);
+
+    if (!findUser) {
+      return res.status(404).json({
+        message: "user not found",
+        success: false,
+      });
+    }
+    let hashpassword;
+    if (password) {
+      hashpassword = await bcrypt.hash(password, 10);
+    }
+
+    findUser.username = username || findUser.username;
+    findUser.email = email || findUser.email;
+    findUser.password = hashpassword || findUser.password;
+
+    findUser.save();
+
+    res.status(201).json({
+      message: "User updated Successfully",
+      success: false,
     });
   } catch (error) {
     res.status(500).json({
--- Web B4 Sec E/Backend/eigth-project/routes/user.js
+++ Web B4 Sec E/Backend/eigth-project/routes/user.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import { roleMiddleware } from "../middleware/roleMiddleware.js";
-import { allUser } from "../contollers/user.js";
+import {
+  allUser,
+  deleteUser,
+  getUser,
+  updateUser,
+} from "../contollers/user.js";
 const router = Router();
 
 router.get("/all", authMiddleware, roleMiddleware("admin"), allUser);
+router.delete("/:userId", authMiddleware, roleMiddleware("admin"), deleteUser);
+router.get("/:userId", authMiddleware, roleMiddleware("admin"), getUser);
+router.put("/:userId", authMiddleware, roleMiddleware("admin"), updateUser);
 
 export default router;
