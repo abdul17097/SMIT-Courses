532f9e22eba5f447e49bd71cbeb342bc70f529ba
abdul17097 <abdul17097@gmail.com>
2026-09-26 05:00:39 -0700

    multer | cloudinary

--- Sect_A_B_Bactch_4/Node_Express_Mongodb/project_4/app.js
+++ Sect_A_B_Bactch_4/Node_Express_Mongodb/project_4/app.js
@@ -2,10 +2,56 @@ import express from "express";
 import { db_connection } from "./config/db_connection.js";
 import { Student } from "./models/student.js";
 import { config } from "dotenv";
+import multer from "multer";
+import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+import { resolve } from "dns";
 
 config();
+const cloudinaryConfig = () => {
+  cloudinary.config({
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUND_NAME,
+  });
+};
+cloudinaryConfig();
+const storage = multer.memoryStorage();
+const uploadMulter = multer({ storage }).single("profile");
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "images");
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqesuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(
+//       null,
+//       file.filename + "-" + uniqesuffix + path.extname(file.originalname),
+//     );
+//   },
+// });
+
+// const upload = multer({
+//   storage: storage,
+// });
+
 const app = express();
+app.use(express.json());
 db_connection();
+
+const uploadCloudinary = async (buffer) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream((error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+    stream.end(buffer);
+  });
+};
 
 app.get("/", async (req, res) => {
   // const students = await Student.find({ age: { $nin: [20, 30, 24] } });
@@ -32,6 +78,16 @@ app.get("/", async (req, res) => {
 });
 
 // 3/15 =
+
+// app.post("/upload",upload.single("profile"), (req, res) => {
+//   console.log(req.body);
+// });
+app.post("/upload", uploadMulter, async (req, res) => {
+  const result = await uploadCloudinary(req.file.buffer);
+  res.json({
+    data: result,
+  });
+});
 
 app.listen(5000, () => {
   console.log("http://localhost:5000");
